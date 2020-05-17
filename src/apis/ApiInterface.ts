8334b9ae3a092a97425bf9dc5a8d93a28f07c27b
kirimin <minkarakirimade@gmail.com>
2020-05-17 14:40:53 +0900

    added token refresh

--- src/apis/ApiInterface.ts
+++ src/apis/ApiInterface.ts
@@ -2,6 +2,7 @@ import Axios from 'axios';
 import { firebaseApp } from '../firebase';
 
 export class ApiInterface {
+  // private domain = 'https://kiriminchan-board-backend.herokuapp.com/';
   private domain = 'http://localhost:8080/';
 
   async getRequest(api: string): Promise<any> {
@@ -11,7 +12,7 @@ export class ApiInterface {
 
   async postRequest(api: string, body: any): Promise<any> {
     const domain = this.domain;
-    const token = await firebaseApp.auth().currentUser!.getIdToken();
+    const token = await firebaseApp.auth().currentUser!.getIdToken(true);
     body['token'] = token;
     return await Axios.post(domain + api, body);
   }
