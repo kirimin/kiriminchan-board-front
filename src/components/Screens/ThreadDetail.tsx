a76bf1f8322137a7c44e5db215cd589ced3d4e82
kirimin <minkarakirimade@gmail.com>
2020-05-17 16:10:06 +0900

    fixed

--- src/components/Screens/ThreadDetail.tsx
+++ src/components/Screens/ThreadDetail.tsx
@@ -1,4 +1,4 @@
-import './threadDetail.css';
+import './ThreadDetail.css';
 import * as React from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { Comment } from '../modules/Comment';
